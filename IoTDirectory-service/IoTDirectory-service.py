f042cbb98f255853084b611d1b2cb3518a710b7f
Vinicio Vieira dos Santos <68493091+VVSRevolution@users.noreply.github.com>
2022-08-10 16:47:52 -0300

    Update DS

--- IoTDirectory-service/IoTDirectory-service.py
+++ IoTDirectory-service/IoTDirectory-service.py
@@ -1,13 +1,10 @@
-import peewee
-import zmq
-import socket
+import peewee,zmq,socket,threading
 from datetime import datetime
 
-PORT = "8080"
-HOST = socket.gethostbyname(socket.gethostname())
-HOST = "127.0.1.1"
-
 FORMAT = 'utf-8'
+PORT = 808080
+HOST = socket.gethostbyname(socket.gethostname())
+HOST = "127.0.1.1" #just for test <------------------- REMOVE THIS LINE
 
 db = peewee.SqliteDatabase('IoTDirectory-service/database_ds.db')
 
@@ -34,44 +31,79 @@ try:
 except:
     print("[DATABASE]:\t[ERRO] ao criar tabela")
 
-context = zmq.Context()
-socket = context.socket(zmq.REP)   
-on = socket.bind("tcp://"+ HOST +":"+ PORT)
-print(on)
 
-while True:
-    message = socket.recv().decode(FORMAT)
-    print("Received request: %s" % message)
-    try:
+def sendIp(): # envia os ADDR dos virtualizers e geteways
+    context = zmq.Context()
+    s = context.socket(zmq.PUB)
+    PortSend = str(PORT + 1)
+    p = f"tcp://{(HOST)}:{PortSend}"
+    print(f"[DIRECTORY SERVICE]:\t Mandando dados em {p}")
+    s.bind(p)
 
-        data = message.split("$")
-        ip = data[1]
-        port = data[2]
-    
-    
-        if(data[0].upper() == "V"):
-            try:
-                virtualizer = Virtualizer.create(
-                    ipVirtualizer = ip,
-                    portVirtualizer = port,
-                    registerTime = datetime.now()
-                )
-                status = f"[DIRECTORY SERVICE]:\tVirtualizer {ip}:{port} foi cadastrado."
-            except:
-                status = f"[DIRECTORY SERVICE]:\tVirtualizer {ip}:{port} já esta cadastrado."
+    query = Virtualizer.select(Virtualizer.ipVirtualizer, Virtualizer.portVirtualizer)
+    virtualizerData = ""
+    for virtualizer in query:
+        virtualizerData += f"{virtualizer.ipVirtualizer}:{virtualizer.portVirtualizer}\n"
 
-        if(data[0].upper() == "G"):
-            try:
-                virtualizer = Gateway.create(
-                    ipGateway = ip,
-                    portGateway = port,
-                    registerTime = datetime.now()
-                )
-                status = f"[DIRECTORY SERVICE]:\tVirtualizer {ip}:{port} foi cadastrado."
-            except:
-                status = f"[DIRECTORY SERVICE]:\tVirtualizer {ip}:{port} já esta cadastrado."
-        socket.send_string(status)
-    except:
-        status ="[DIRECTORY SERVICE]:\t ERROR estrada não formatada corretamente"
-        socket.send_string(status)
+    query = Gateway.select(Gateway.ipGateway, Gateway.portGateway)
+    gatewayData = ""
+    for gateway in query:
+        gatewayData += f"{gateway.ipGateway}:{gateway.portGateway}\n"
+
+    #testar saida de dados 
+    #print(virtualizerData) 
+    #print(gatewayData)
+
+
+    s.send_string("virtualizer" + virtualizerData)
+    s.send_string("gateway" + gatewayData)
+
+
+def getIp(): # recebe os ADDR dos virtualizers e geteways
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)   
+    p = "tcp://"+ HOST +":"+ str(PORT)
+    print(f"[DIRECTORY SERVICE]:\t Recebendo dados em {p}")
+    socket.bind(p)
+
+    while True:
+        message = socket.recv().decode(FORMAT)
+        print("Received request: %s" % message)
+        try:
+
+            data = message.split("$")
+            ip = data[1]
+            port = data[2]
+        
+        
+            if(data[0].upper() == "V"):
+                try:
+                    virtualizer = Virtualizer.create(
+                        ipVirtualizer = ip,
+                        portVirtualizer = port,
+                        registerTime = datetime.now()
+                    )
+                    status = f"[DIRECTORY SERVICE]:\tVirtualizer {ip}:{port} foi cadastrado."
+                except:
+                    status = f"[DIRECTORY SERVICE]:\tVirtualizer {ip}:{port} já esta cadastrado."
+
+            if(data[0].upper() == "G"):
+                try:
+                    virtualizer = Gateway.create(
+                        ipGateway = ip,
+                        portGateway = port,
+                        registerTime = datetime.now()
+                    )
+                    status = f"[DIRECTORY SERVICE]:\tVirtualizer {ip}:{port} foi cadastrado."
+                except:
+                    status = f"[DIRECTORY SERVICE]:\tVirtualizer {ip}:{port} já esta cadastrado."
+            socket.send_string(status)
+        except:
+            status ="[DIRECTORY SERVICE]:\t ERROR estrada não formatada corretamente"
+            socket.send_string(status)
     
+
+sendIP = threading.Thread(target = sendIp)
+getIP = threading.Thread(target = getIp)
+getIP.start()
+sendIP.start()
